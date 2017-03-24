56ae1d957a51b6bce46616c4651cacce112db124
Sose97 <Sose97@users.noreply.github.com>
2017-03-24 18:01:22 +0400

    static pholimorfizm

---
+++ Homeworks/Homework2/Pholimorphizm/Pholomorfizm/Pholomorfizm/BaseClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pholomorfizm
+{
+   public class BaseClass
+    {
+        public void method1()
+        {
+            Console.WriteLine("Base method");
+        }
+    };
+
+    public class DeliveredClass:BaseClass
+    {
+        public void method1(string x)
+        {
+            Console.WriteLine(x+"Delivered metod");
+        }
+    }
+}
---
+++ Homeworks/Homework2/Pholimorphizm/Pholomorfizm/Pholomorfizm/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Activities;
+using System.Activities.Statements;
+
+namespace Pholomorfizm
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseClass temp1 = new BaseClass();
+            BaseClass temp2 = new DeliveredClass();
+            DeliveredClass temp3 = new DeliveredClass();
+
+            temp1.method1();
+            temp2.method1();
+            temp3.method1();
+            temp3.method1("Hello");
+        }
+    }
+}
